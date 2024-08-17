d9d37ba7dca52cd5460d85317c8c241ec988bb51
minggaa <t4b.ming@gmail.com>
2024-08-17 15:36:21 +0800

    Updated: - Repositioning of command variable from local to each condition block, to be global.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,28 +1,56 @@
-const { Events } = require('discord.js');
+const { Events, inlineCode } = require('discord.js');
+
+const bot = require('../utils/bot.js');
+const colors = bot.colors;
+const errorLog = bot.errorLog;
 
 // Receive command interactions.
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        
-        if (!interaction.isChatInputCommand()) return; // to handle only slash commands
 
+        const errorEmbed = (title, description) => bot.errorEmbed(errorLog, title, description);
         const command = interaction.client.commands.get(interaction.commandName);
-    
-        if (!command) { // handles when the command is not found.
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-    
-        try { // executes the command with provided arguments and sends response back to user.
-            await command.execute(interaction);
-        } catch (error) { // handles any errors that occur during execution.
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'An error occured while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'An error occured while executing this command!', ephemeral: true });
+
+        // Handles when the command is not found.
+        const checkCommand = (command) => {
+            if (!command) { 
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return false;
+            };
+            return true;
+        };
+
+        if (interaction.isChatInputCommand()) {
+            const commandName = command.data.name;
+            if (!checkCommand(command)) return;
+        
+            try { // executes the command with provided arguments and sends response back to user.
+                await command.execute(interaction);
+            } catch (error) {
+                console.error('INTERACTION CREATE ERROR:\n', error);
+                errorLog.push(error);
+
+                const properties = {
+                    content: errorMessage,
+                    embeds: [errorEmbed('An error occured while executing this command!', `Use ${inlineCode(`/${commandName}`)} to try again.`)],
+                    ephemeral: true,
+                };
+                
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp(properties);
+                } else {
+                    await interaction.reply(properties);
+                }
             }
-        }
+        } else if (interaction.isAutocomplete()) {
+            if (!checkCommand(command)) return;
+    
+            try {
+                await command.autocomplete(interaction);
+            } catch (error) {
+                console.error(error);
+            }
+        };
     },
 };
